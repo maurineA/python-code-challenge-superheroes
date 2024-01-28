08cb3efa922fbae33b4f63cc7b87a38c68cbe51c
maurineA <maurinawambuia@gmail.com>
2024-01-28 22:00:17 +0300

    created relationships

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -8,6 +8,8 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
+    heropowers = db.relationship('HeroPower', backref='heros')
+
 
 
 class Power(db.Model):
@@ -15,12 +17,14 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.Text)
+    heropowers = db.relationship('HeroPower', backref='powers')
+
     
 
 class HeroPower(db.Model):
     __tablename__ = 'heropowers'
 
     id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
-    power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
+    hero_id = db.Column(db.Integer, db.ForeignKey('heros.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
     strength = db.Column(db.String)
