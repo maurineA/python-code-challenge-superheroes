501c72696bb73cc0cd2de5c11916a6fd354a75f3
maurineA <maurinawambuia@gmail.com>
2024-01-28 21:48:58 +0300

    created table heros

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -3,8 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heros'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
+
+
+class Power(db.Model):
+    __tablename__ = 'powers'
