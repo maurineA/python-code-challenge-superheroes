9a4a8804dbdf85d626cf1de10a3fec1dabfd29d9
maurineA <maurinawambuia@gmail.com>
2024-01-28 21:57:40 +0300

    created heropowers table

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -15,3 +15,12 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.Text)
+    
+
+class HeroPower(db.Model):
+    __tablename__ = 'heropowers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
+    strength = db.Column(db.String)
